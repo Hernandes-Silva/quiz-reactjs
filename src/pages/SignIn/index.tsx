c9a25630c584a5ba1241aa42218b895e6a01d9d9
Hernandes-Silva <lertanisslobr@gmail.com>
2022-05-17 19:33:28 -0300

    feat: criando as pages de signin e signup

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,16 @@
+// @flow 
+import * as React from 'react';
+type Props = {
+    
+};
+
+const Login = (props: Props) => {
+    
+    return (
+        <div>
+            <p>Login</p>
+        </div>
+    );
+};
+
+export default Login;

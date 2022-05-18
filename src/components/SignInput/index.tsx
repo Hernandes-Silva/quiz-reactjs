b834cd8247bdfc316a5506ded17528ddfba7f06b
Hernandes-Silva <lertanisslobr@gmail.com>
2022-05-18 11:27:59 -0300

    feat(components/SignInput):criando o component SignInput

---
+++ src/components/SignInput/index.tsx
@@ -0,0 +1,15 @@
+// @flow 
+import * as React from 'react';
+import * as S from './styles';
+type Props = {
+    
+};
+
+const SignInput: React.FC = (props: Props) => {
+    return (
+        <S.InputArea >
+            <p>aaa</p>
+        </S.InputArea>
+    );
+};
+export default SignInput;
---
+++ src/components/SignInput/styles.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const InputArea = styled.div`
+    width:100%;
+    height:60px;
+    background-color: #EBF2FA;
+    border-radius:30px;
+    padding:10px;
+    color:#05668D;
+    display:flex;
+    align-items:center;
+`

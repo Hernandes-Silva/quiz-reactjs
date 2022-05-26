f3ad8ae1b5868f8fbca268a12f7fde7f906d28a5
Hernandes-Silva <lertanisslobr@gmail.com>
2022-05-26 10:50:07 -0300

    feat:fazendo os style do GlobalStyled

--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -1,14 +1,24 @@
 import { createGlobalStyle } from "styled-components";
+//Mobile com 24px, Tablet com 18px e Web com 16px.
 
 export default createGlobalStyle`
     * {
         margin: 0;
         padding:0;
         box-sizing: border-box;
+
     }
     body {
-        font-size: 14px;
-        color:#EBF2FA;
         font-family: sans-serif;
+        color:#EBF2FA;
+        font-size: 24px !important;
+
+        @media (min-width: 768px) {
+            font-size: 18px !important;
+        }
+
+        @media (min-width: 1024px) {
+            font-size: 16px !important;
+        }
     }
 ` 

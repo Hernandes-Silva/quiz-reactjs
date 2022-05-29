84be31233555cf4bec0444b9b32d1bc4fc8c4fc2
Hernandes-Silva <lertanisslobr@gmail.com>
2022-05-29 09:25:10 -0300

    feat: modificando o component ButtonOpacity

--- src/components/ButtonOpacity/index.tsx
+++ src/components/ButtonOpacity/index.tsx
@@ -2,21 +2,20 @@
 import * as React from 'react';
 import * as S from './styles'
 type Props = {
-    children: React.ReactNode
+    children: React.ReactNode;
+    onClick: () => void;
 };
 
 
 export const ButtonOpacity = (props: Props) => {
     const [touched, touchedSet] = React.useState<boolean>(false)
-    const handleClick = () => {
-        return
-    }
+
     return (
             <S.CustomButtom 
             style={{ opacity: touched ? 0.5 : 1, transition: 'opacity 100ms ease' }}
             onMouseDown={() => touchedSet(true)}
             onMouseUp={() => touchedSet(false)}
-            onClick={() => handleClick()} >
+            onClick={props.onClick} >
                     {props.children}
             </S.CustomButtom>
     );

18900510f33728d3ab225131d0457ca64ee98946
Hernandes-Silva <lertanisslobr@gmail.com>
2022-05-29 09:27:21 -0300

    feat: criando uma função para pegar erros do request

---
+++ src/utils/getRequestErrorMessage.ts
@@ -0,0 +1,11 @@
+const getRequestErrorMessage = (err: any): string => {
+
+    const message =
+        err.response?.data?.detail ??
+        err.message ??
+        `An unexpected error has occurred, we are working to resolve it.`
+
+    return message
+}
+
+export default getRequestErrorMessage;

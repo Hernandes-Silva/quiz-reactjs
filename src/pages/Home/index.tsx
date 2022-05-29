0d3976f706eb4bc0a07a5198401bfd7a849cbb7d
Hernandes-Silva <lertanisslobr@gmail.com>
2022-05-29 09:25:59 -0300

    feat(Home): criando o a page Home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+// @flow 
+import * as React from 'react';
+type Props = {
+
+};
+const Home: React.FC =  (props: Props) => {
+    return (
+        <div>
+            <h1>Helloo Home</h1>
+        </div>
+    );
+};
+
+export default Home;

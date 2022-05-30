0bd8d622b26d54edb0e312e61849af13250a7afd
Hernandes-Silva <lertanisslobr@gmail.com>
2022-05-30 12:06:39 -0300

    feat(src/pages/SignIn) colocando reack hook form no SignIn

--- src/pages/SignIn/components/LoginForm/index.tsx
+++ src/pages/SignIn/components/LoginForm/index.tsx
@@ -2,60 +2,65 @@
 import * as React from 'react';
 import ButtonOpacity from '../../../../components/ButtonOpacity';
 import SignInput from '../../../../components/SignInput';
-import {Error } from '../../styles'
+import { Error } from '../../styles'
 import lock from '../../../../assets/lock.svg'
 import email from '../../../../assets/email.svg'
 import { useAuthContext } from '../../../../contexts/authProvider';
 import { InputArea } from '../../../../styles/styleds';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { PropsDoSignIn } from '../../../../services/axios/modules/authentication/types';
 
 type Props = {
 
 };
 export const LoginForm: React.FC = (props: Props) => {
-    const [usernameField, setUsernameField] = React.useState<string>("");
-    const [passwordField, setPasswordield] = React.useState<string>("");
+    
     const [error, setError] = React.useState<string>("");
     const { handleSignIn } = useAuthContext();
+    const {register, handleSubmit } = useForm<PropsDoSignIn>();
 
-    const login = async () => {
+    const login: SubmitHandler<PropsDoSignIn> = async (data) => {
+        
         setError('')
-        if(!usernameField || !passwordField){
+        if (!data.username || !data.password) {
             setError('Username or password cannot be empty')
             return
         }
-        const e = await handleSignIn({username: usernameField, password:passwordField})
         
-        if(!e) return
+        const e = await handleSignIn(data)
+
+        if (!e) return
 
         setError(e);
     }
 
     return (
         <InputArea>
-
             {error &&
-            <Error>
-                {error}
-            </Error>
+                <Error>
+                    {error}
+                </Error>
             }
-            <SignInput
-                icon={email}
-                placeholder="Digite seu email"
-                value={usernameField}
-                onChange={e => setUsernameField(e.target.value)}
-            />
+            
+            <form onSubmit={handleSubmit(login)}>
+                <SignInput
+                    icon={email}
+                    placeholder="Digite seu email"
+                    {...register("username")}
+                />
 
-            <SignInput
-                icon={lock}
-                type="password"
-                placeholder="Digite sua senha"
-                value={passwordField}
-                onChange={e => setPasswordield(e.target.value)}
-            />
+                <SignInput
+                    icon={lock}
+                    type="password"
+                    placeholder="Digite sua senha"
+                    {...register("password")}
+                    
+                />
 
-            <ButtonOpacity onClick={login} >
-                LOGIN
-            </ButtonOpacity>
+                <ButtonOpacity type='submit' >
+                    LOGIN
+                </ButtonOpacity>
+            </form>
         </InputArea>
     );
 };

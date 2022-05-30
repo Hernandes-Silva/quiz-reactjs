1b3e5bfb43baa3a32fb9ea6ed7d37ae1c5a4b25a
Hernandes-Silva <lertanisslobr@gmail.com>
2022-05-30 12:07:40 -0300

    feat(src/pages/SignUp) criando a page SignUp

---
+++ src/pages/SignUp/components/SignUpForm/index.tsx
@@ -0,0 +1,73 @@
+// @flow 
+import * as React from 'react';
+import SignInput from '../../../../components/SignInput';
+import lock from '../../../../assets/lock.svg'
+import email from '../../../../assets/email.svg'
+import { InputArea } from '../../../../styles/styleds';
+import ButtonOpacity from '../../../../components/ButtonOpacity';
+import { SubmitHandler, useForm } from 'react-hook-form';
+type Props = {
+
+};
+type FormValues = {
+    username: string;
+    first_name: string;
+    last_name: string;
+    email:string;
+    password: string;
+    confirm_password: string;
+    
+};
+export const SignUpForm: React.FC = (props: Props) => {
+    const { register, handleSubmit } = useForm<FormValues>()
+
+
+    const signUp: SubmitHandler<FormValues> = async (data) => {
+        console.log(data)
+    }
+    return (
+        <InputArea>
+            <form onSubmit={handleSubmit(signUp)}>
+                <SignInput
+                    icon={email}
+                    placeholder="Username"
+                    {...register("username")}
+
+                />
+                <SignInput
+                    icon={email}
+                    placeholder="Primeiro nome"
+                    {...register("first_name")}
+
+                />
+                <SignInput
+                    icon={email}
+                    placeholder="Sobrenome"
+                    {...register("last_name")}
+
+                />
+                <SignInput
+                    icon={email}
+                    placeholder="Email"
+                    {...register("email")}
+
+                />
+                <SignInput
+                    icon={lock}
+                    type="password"
+                    placeholder="Digite sua senha"
+                    {...register("password")}
+
+                />
+                <SignInput
+                    icon={lock}
+                    type="password"
+                    placeholder="Confirme sua senha"
+                    {...register("confirm_password")}
+
+                />
+                <ButtonOpacity type='submit'>SIGN UP</ButtonOpacity>
+            </form>
+        </InputArea>
+    );
+};
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,1 +1,18 @@
-export {};
+// @flow 
+import * as React from 'react';
+import { Container } from '../../styles/styleds';
+import { SignUpForm } from './components/SignUpForm';
+import { Title } from './styles';
+type Props = {
+    
+};
+export const SignUp: React.FC =  (props: Props) => {
+    return (
+        <Container>
+            <Title>SIGN UP</Title>
+            <SignUpForm />
+
+        </Container>
+    );
+};
+export default SignUp;

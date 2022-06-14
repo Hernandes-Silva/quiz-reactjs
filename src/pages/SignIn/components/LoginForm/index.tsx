2f83d0341e88e171f6f18200b320d80b8b35ecd9
Hernandes-Silva <lertanisslobr@gmail.com>
2022-06-14 09:28:04 -0300

    feat(src/pages/SignIn): pegando o historico de rotas para que apos o login ele seja redirecionado para a última pagina acessada

--- src/pages/SignIn/components/LoginForm/index.tsx
+++ src/pages/SignIn/components/LoginForm/index.tsx
@@ -11,24 +11,38 @@ import { SubmitHandler, useForm } from 'react-hook-form';
 import { PropsDoSignIn } from '../../../../services/axios/modules/authentication/types';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup"
+import { useLocation, useNavigate } from 'react-router-dom';
+import paths from '../../../../routes/paths';
 
 const schema = yup.object({
     username: yup.string().min(6).required(),
     password: yup.string().min(6).required(),
 }).required();
-
+type LocationProps = {
+    state: {
+      from: Location;
+    };
+};
 export const LoginForm: FC = () => {
     
     const [error, setError] = useState<string>("");
     const { handleSignIn, validateAuth, isLogged } = useAuthContext();
+
+    const navigate = useNavigate()
+    const location = useLocation() as unknown as LocationProps;
+    const from = location.state?.from.pathname || paths.HOME;
     const {register, handleSubmit, formState: {errors} } = useForm<PropsDoSignIn>({
         resolver: yupResolver(schema)
     });
 
     useEffect(() => {
-        if(isLogged){
-            validateAuth();
+        const verifyLogin = async() =>{
+            if(isLogged){
+               const logged = await validateAuth();
+               if(logged) navigate(from)
+            }
         }
+        verifyLogin()
     }, [])
 
     const login: SubmitHandler<PropsDoSignIn> = async (data) => {
@@ -39,11 +53,14 @@ export const LoginForm: FC = () => {
             return
         }
         
-        const e = await handleSignIn(data)
+        const message = await handleSignIn(data)
 
-        if (!e) return
+        if (message === "success"){
+            navigate(from)
+            return
+        } 
 
-        setError(e);
+        setError(message);
     }
 
     return (
